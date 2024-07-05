3050409d37c3bba5b2f6c4ebece3e524a609afb8
hiltas73 <h_iltas@yahoo.com>
2024-07-05 12:06:35 +0300

    ProductPage.java ADDED

---
+++ src/test/java/com/cydeo/pages/ProductPage.java
@@ -0,0 +1,20 @@
+package com.cydeo.pages;
+
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.WebElement;
+
+public class ProductPage extends BasePage{
+
+    public ProductPage(String env) {
+        super(env);
+    }
+    @AndroidFindBy(xpath = "(//android.view.ViewGroup[@content-desc=\"test-ADD TO CART\"])[1]")
+    public WebElement firstProduct;
+
+    @AndroidFindBy(xpath = "//*[@text='REMOVE'] ")
+    public WebElement removeElement;
+
+    @AndroidFindBy(xpath = "//android.view.ViewGroup[@content-desc=\"test-Cart\"]/android.view.ViewGroup/android.widget.TextView")
+    public WebElement numberOfProducts;
+
+}
