01ae2c8930917a51c8c3435779eddf5c009d2f5b
hiltas73 <h_iltas@yahoo.com>
2024-07-05 12:01:09 +0300

    Login method ADDED

---
+++ src/test/java/com/cydeo/pages/BasePage.java
@@ -0,0 +1,13 @@
+package com.cydeo.pages;
+
+import com.cydeo.utils.Driver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+
+    public BasePage(String env){
+        PageFactory.initElements(new AppiumFieldDecorator(Driver.getDriver(env+"-sauceApp")),this);
+    }
+
+}
---
+++ src/test/java/com/cydeo/pages/LoginPage.java
@@ -0,0 +1,27 @@
+package com.cydeo.pages;
+
+
+import com.cydeo.utils.ConfigurationReader;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.WebElement;
+public class LoginPage extends BasePage{
+
+    public LoginPage(String env) {
+        super(env);
+    }
+    @AndroidFindBy(accessibility = "test-Username")
+    public WebElement userName;
+
+    @AndroidFindBy(accessibility = "test-Password")
+    public WebElement password;
+
+    @AndroidFindBy(accessibility = "test-LOGIN")
+    public WebElement loginButton;
+
+    public void login(){
+        userName.sendKeys(ConfigurationReader.getProperty("userName"));
+        password.sendKeys(ConfigurationReader.getProperty("password"));
+        loginButton.click();
+    }
+
+}
