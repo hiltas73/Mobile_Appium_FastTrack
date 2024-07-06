72663397d53ef5e85d2fae1c55dba6acbd697d29
hiltas73 <h_iltas@yahoo.com>
2024-07-06 11:26:03 +0300

    remote run scenario and driver setup last version ADDED

--- src/test/java/com/cydeo/utils/Driver.java
+++ src/test/java/com/cydeo/utils/Driver.java
@@ -43,52 +43,70 @@ public class Driver {
                 case "remote-android-sauceApp":
                     MutableCapabilities caps = new MutableCapabilities();
                     caps.setCapability("platformName", "Android");
-                    // change Application part
-                    caps.setCapability("appium:app", "https://github.com/saucelabs/sample-app-mobile/releases/download/2.7.1/Android.SauceLabs.Mobile.Sample.app.2.7.1.apk");
-                    caps.setCapability("appium:deviceName", "Google Pixel 3 GoogleAPI Emulator");
-                    caps.setCapability("appium:platformVersion", "11.0");
+                    caps.setCapability("appium:deviceName", "Android GoogleAPI Emulator");
+                    caps.setCapability("appium:platformVersion", "current_major");
                     caps.setCapability("appium:automationName", "UiAutomator2");
-                    caps.setCapability("appPackage","com.swaglabsmobileapp");
-                    caps.setCapability("appActivity","com.swaglabsmobileapp.MainActivity");
-                    // add app activity and package
                     MutableCapabilities sauceOptions = new MutableCapabilities();
-                    sauceOptions.setCapability("appiumVersion", "2.0.0");
-                    // username and access key is unique to the user
-                    sauceOptions.setCapability("username", "secret-user");
-                    sauceOptions.setCapability("accessKey", "super-secret-password");
-                    sauceOptions.setCapability("build", "sauceLab123");
-                    sauceOptions.setCapability("name", "smokeTest");
+                    sauceOptions.setCapability("username", "oauth-hiltas73-XXXXX");
+                    sauceOptions.setCapability("accessKey", "super-secret-key");
+                    sauceOptions.setCapability("build", "Test001");
+                    sauceOptions.setCapability("name", "Android_Test");
                     sauceOptions.setCapability("deviceOrientation", "PORTRAIT");
                     caps.setCapability("sauce:options", sauceOptions);
 
+// Start the session
                     try {
-                        url = new URL("https://ondemand.us-west-1.saucelabs.com:443/wd/hub");
+                        url = new URL("https://ondemand.eu-central-1.saucelabs.com:443/wd/hub");
                     } catch (MalformedURLException e) {
                         throw new RuntimeException(e);
                     }
                     driver = new AndroidDriver(url, caps);
+
+// replace with commands and assertions
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    String jobStatus = "passed"; // or "failed"
+
+// end the session
+                    // driver.executeScript("sauce:job-result=" + jobStatus);
+                    driver.quit();
                     break;
                 case "remoteIOS-sauceApp":
                     MutableCapabilities capsI = new MutableCapabilities();
                     capsI.setCapability("platformName", "iOS");
-                    capsI.setCapability("appium:app", "https://github.com/saucelabs/sample-app-mobile/releases/download/2.7.1/iOS.RealDevice.SauceLabs.Mobile.Sample.app.2.7.1.ipa");  // The filename of the mobile app
-                    capsI.setCapability("appium:deviceName", "iPhone.*");
+                    capsI.setCapability("appium:deviceName", "iPhone Simulator");
+                    capsI.setCapability("appium:platformVersion", "current_major");
                     capsI.setCapability("appium:automationName", "XCUITest");
                     MutableCapabilities sauceOptionsI = new MutableCapabilities();
-
-                    sauceOptionsI.setCapability("username", "secret-user");
-                    sauceOptionsI.setCapability("accessKey", "super-secret-password");
-                    sauceOptionsI.setCapability("build", "Test123");
-                    sauceOptionsI.setCapability("name", "IOS_Test");
+                    sauceOptionsI.setCapability("username", "oauth-hiltas73-XXXXX");
+                    sauceOptionsI.setCapability("accessKey", "super-secret-key");
+                    sauceOptionsI.setCapability("build", "<your build id>");
+                    sauceOptionsI.setCapability("name", "<your test name>");
                     sauceOptionsI.setCapability("deviceOrientation", "PORTRAIT");
                     capsI.setCapability("sauce:options", sauceOptionsI);
+
+// Start the session
                     try {
-                        url = new URL("https://ondemand.us-west-1.saucelabs.com:443/wd/hub");
+                        url = new URL("https://ondemand.eu-central-1.saucelabs.com:443/wd/hub");
                     } catch (MalformedURLException e) {
                         throw new RuntimeException(e);
                     }
-                    driver = new IOSDriver(url, capsI);
-                    break;
+                    IOSDriver driver = new IOSDriver(url, capsI);
+
+// replace with commands and assertions
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    // String jobStatus = "passed"; // or "failed"
+
+// end the session
+                    //driver.executeScript("sauce:job-result=" + jobStatus);
+                    driver.quit();
             }
 
         }
